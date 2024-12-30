accbd987386ddc5560fc6f122fa2e1b5c19ab31c
tunggulalmajid <tunggulmajid@gmail.com>
2024-12-31 01:17:29 +0700

    end of v3

---
+++ CsharpV3/Hero.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace CsharpV3
+{
+    class Hero
+    {
+        public string color = "yellow";
+        public int power = 1000;
+        public int health = 1500;
+        public void kekuatan()
+        {
+            Console.WriteLine("terbang");
+        }
+
+    }
+}
--- CsharpV3/Program.cs
+++ CsharpV3/Program.cs
@@ -10,7 +10,14 @@ namespace CsharpV3
     {
         static void Main(string[] args)
         {
+            Hero superman = new Hero();
+            Console.WriteLine("superman berwarna >> {0}", superman.color);
+            Console.WriteLine("superman mempunyai power >> {0}",superman.power);
+            Console.WriteLine("superman mempunyai health sebesar >> {0}", superman.health);
+            Console.Write("superman memiliki kekuatan >> ");
+            superman.kekuatan();
 
+            Console.ReadLine();
         }
     }
 }
